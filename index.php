eb048fed05d1d2575881c7a7af8bace074205982
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-12-04 23:41:01 +0800

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-s
+<?php
+$client_id = '64db9b34101d94173ad9';
+$redirect_uri = 'http://localhost:3000/ghlogin.php';
+
+echo "<a href='https://github.com/login/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri'>Login with GitHub</a>";
+?>
+
