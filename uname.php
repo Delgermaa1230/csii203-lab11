c95c78342edb734116ca504be5cc61b076c87255
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-12-14 01:42:45 +0800

    uname

--- uname.php
+++ uname.php
@@ -1,20 +1,57 @@
 <?php
-session_start();
 
-$client_id = '64db9b34101d94173ad9';
-$client_secret = 'YOUR_CLIENT_SECRET'; // Replace with your actual client secret
 
-if (isset($_SESSION['user_email'])) {
-    $user_email = $_SESSION['user_email'];
+$curl = curl_init();
 
-    $allowed_emails = array('alloweduser@example.com', 'anotherallowed@example.com');
+$accessToken = $_COOKIE['access_token'];
 
-    if (in_array($user_email, $allowed_emails)) {
-        echo "Welcome, $user_email! Your special information is here.";
-    } else {
-        echo 'Sorry, you are not allowed to access this information.';
+curl_setopt($curl, CURLOPT_URL, 'https://api.github.com/user/emails');
+// curl_setopt($curl, CURLOPT_POST, true);
+
+$headers = [
+    'Accept: application/json',
+    'Authorization: Bearer ' . $accessToken,
+    'X-GitHub-Api-Version: 2022-11-28',
+    'User-Agent: mybrowser'
+];
+
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($curl, CURLOPT_, http_build_query([
+//     'client_id' => $CLIENT_ID,
+//     'client_secret' => $CLIENT_SECRET,
+//     'code' => $sessionCode,
+// ]));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+$emails_jarr = json_decode($result, true);
+
+$emails = [];
+
+foreach ($emails_jarr as $item) {
+    if (isset($item['email'])) {
+        $emails[] = $item['email'];
     }
-} else {
-    echo 'Error: User not authenticated.';
 }
+
+
 ?>
+
+<div> Current user email addressess:
+
+    <?php
+    $emailList = '<ul>';
+    foreach ($emails as $email) {
+        $emailList .= '<li>' . $email . '</li>';
+    }
+    $emailList .= '</ul>';
+
+    echo $emailList;
+    ?>
+
+
+    <!-- <?php print_r(implode(',', $emails)) ?></div> -->
+
+    <!-- <pre><?php print_r($result) ?></pre> -->
