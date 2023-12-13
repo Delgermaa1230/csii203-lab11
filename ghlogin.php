64473e17a08f77539e55426b1eaf905e7bfe4a58
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-12-14 03:44:40 +0800

    тусгай хаягт мэдээдлэл рауулах

--- ghlogin.php
+++ ghlogin.php
@@ -27,15 +27,223 @@ $jresult = json_decode($result, true);
 $accessToken = $jresult['access_token'];
 
 setcookie('access_token', $accessToken, 0, '/');
-// echo "the code is " + $sessionCode;
 
+if (!isset($_COOKIE['access_token'])) {
+    header("Location: http://localhost:3000/index.php"); 
+    exit();
+}
+
+$accessToken = $_COOKIE['access_token'];
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, 'https://api.github.com/user/emails');
+$headers = [
+    'Accept: application/json',
+    'Authorization: Bearer ' . $accessToken,
+    'X-GitHub-Api-Version: 2022-11-28',
+    'User-Agent: mybrowser'
+];
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+$emails_jarr = json_decode($result, true);
+
+$allowedEmails = ["Galbayardelgermaa@gmail.com"];
+$userEmails = [];
+
+foreach ($emails_jarr as $item) {
+    if (isset($item['email'])) {
+        $userEmails[] = $item['email'];
+    }
+}
+
+if (!array_intersect($userEmails, $allowedEmails)) {
+    die("Access Denied"); 
+}
 ?>
 
-<!-- <pre>
+<!DOCTYPE html>
+<html lang="en">
 
-    <?php
-    echo $result;
-    ?>
-</pre> -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demi's</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #ffffff;
+            margin: 20px;
+            padding: 0;
+        }
 
-<span></span>
+        h1 {
+            text-align: center;
+            color: #9f673b;
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #9f673b;
+            font-size: 14px;
+        }
+
+
+        .input-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        input {
+            padding: 12px;
+            margin-bottom: 16px;
+            width: 220px;
+            border: 2px solid #9f673b;
+            border-radius: 8px;
+            font-size: 14px;
+            outline: none;
+        }
+
+        button {
+            padding: 14px;
+            background-color: #9f673b;
+            color: #fafafa;
+            border: none;
+            cursor: pointer;
+            border-radius: 8px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+        }
+
+        button:hover {
+            background-color: #9f673b;
+        }
+
+        #displayArea {
+            margin-top: 20px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            overflow: hidden;
+        }
+
+        th,
+        td {
+            padding: 14px;
+            text-align: left;
+            border-bottom: 1px solid #9f673b;
+        }
+
+        th {
+            background-color: #9f673b;
+            color: #fff;
+        }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</head>
+
+<body>
+
+    <h1>Demi-гийн Эд зүйлсийн мэдээллийн сан</h1>
+
+    <label for="stuffIdInput">Эд зүйлийн дугаарыг оруулна уу :</label>
+    <input type="number" id="stuffIdInput" min="1" max="101" />
+    <button onclick="fetchData()">Сонгосон эд зүйлийг харах</button>
+    <button onclick="fetchAllData()">Бүх эд зүйлсийг харах</button>
+    <button onclick="fetchBookData()">Бүх номнуудыг харах</button>
+
+
+    <div id="displayArea"></div>
+
+    <script>
+        function fetchData() {
+            var selectedStuffId = $("#stuffIdInput").val();
+
+
+            if (isNaN(selectedStuffId)) {
+                alert("Эд зүйлийн дугаар алдаатай байна!");
+                return;
+            }
+
+            if (selectedStuffId < 1 || selectedStuffId > 101) {
+                alert("Эд зүйлийн дугаар 1 ээс 100 гийн хооронд байх ёстой шүү!");
+                return;
+            }
+
+            $.ajax({
+                url: 'details.php',
+                type: 'GET',
+                data: { stuffid: selectedStuffId },
+                dataType: 'json',
+                success: function (data) {
+                    displayData(data);
+                },
+                error: function (error) {
+                    console.error('Өгөгдөл авахад алдаа гарлаа:', error);
+                }
+            });
+        }
+
+        function fetchAllData() {
+            $.ajax({
+                url: 'fetchalldata.php', 
+                success: function (data) {
+                    displayData(data);
+                },
+                error: function (error) {
+                    console.error('Бүх өгөгдөл авахад алдаа гарлаа:', error);
+                }
+            });
+        }
+
+        function fetchBookData() {
+            $.ajax({
+                url: 'fetchbookdata.php', 
+                success: function (data) {
+                    displayData(data);
+                },
+                error: function (error) {
+                    console.error('Номын өгөгдөл авахад алдаа гарлаа:', error);
+                }
+            });
+        }
+
+
+        function displayData(data) {
+            var displayArea = $('#displayArea');
+            displayArea.empty();
+
+            var table = '<table border="1"><tr><th>Эд зүйлийн дугаар</th><th>Эд зүйлийн нэр</th><th>Худалдаж авсан өдөр</th><th>Зориулалт</th><th>Үнэ $</th><th>Ангилал</th></tr>';
+
+            for (var i = 0; i < data.length; i++) {
+                table += '<tr>';
+                table += '<td>' + data[i].stuffId + '</td>';
+                table += '<td>' + data[i].stuffName + '</td>';
+                table += '<td>' + data[i].stuffBdate + '</td>';
+                table += '<td>' + data[i].stuffusage + '</td>';
+                table += '<td>' + data[i].stuffPrice + '</td>';
+                table += '<td>' + data[i].categoryName + '</td>';
+                table += '</tr>';
+            }
+
+            table += '</table>';
+            displayArea.html(table);
+        }
+
+    </script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,23 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GitHub API Authorization</title>
+    <title>Demi's GitHub API Authorization</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            text-align: center;
+            background-color: #fff;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #9f673b;
+            margin-bottom: 20px;
+        }
+
+        p {
+            color: #555;
+            margin-bottom: 30px;
+        }
+
+        a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #9f673b;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        a:hover {
+            background-color: #74512b;
+        }
+
+        .note {
+            color: #777;
+            font-size: 14px;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <p>
-        Well, hello there!
-    </p>
-    <p>
-        <?php
-        $client_id = 'febe1cf8420d5e54ed43';
-        ?>
-        We're going to now talk to the GitHub API. Ready?
-        <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=<?php echo $client_id; ?>">Click here</a> to begin!
-    </p>
-    <p>
-        If that link doesn't work, remember to provide your own <a href="/apps/building-oauth-apps/authorizing-oauth-apps/">Client ID</a>!
-    </p>
+    <div class="container">
+        <h1>Welcome to Demi's GitHub API Authorization</h1>
+        <p>Дэмигийн Эд зүйлсийн бүртгэлд нэвтрэх гэж байна ^^</p>
+        <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=febe1cf8420d5e54ed43">GitHub хаягаар хандах</a>
+        <!--<p class="note">If that link doesn't work, remember to provide your own <a href="/apps/building-oauth-apps/authorizing-oauth-apps/">Client ID</a>!</p>-->
+    </div>
 </body>
 
 </html>
